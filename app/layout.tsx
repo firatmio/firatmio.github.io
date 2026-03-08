38dfe96fb3727b7019422ae03aa47e5561e1f03f
Fırat Tuna Arslan <firattunaarslan@gmail.com>
2026-03-08 20:27:30 +0300

    refactor: remove unused SVG files and update HTML language attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" translate="no">
       <body className={`${spaceGrotesk.variable} ${jetbrainsMono.variable}`}>
         <GSAPProvider>
           <CustomCursor />
